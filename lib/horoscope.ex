8311965e0128d2411bbf5960d21eb0c47327ea32
Kash Nouroozi <knouroozi@gmail.com>
2015-09-05 15:05:17 -0400

    Standalone :)

--- lib/horoscope.ex
+++ lib/horoscope.ex
@@ -6,15 +6,14 @@ defmodule Horoscope do
   def worker_pool_config do
     [worker_module: Horoscope.Worker,
      name: {:local, worker_pool},
-     size: 32,
-     max_overflow: 8]
+     size: 4,
+     max_overflow: 2]
   end
 
   def start(_type, _args) do
     import Supervisor.Spec
 
     children = [
-      # worker(__MODULE__, [], function: :server),
       worker(Horoscope.Repo, []),
       :poolboy.child_spec(worker_pool, worker_pool_config, [])
     ]
@@ -26,11 +25,6 @@ defmodule Horoscope do
 
     Supervisor.start_link(children, options)
   end
-
-  # def server do
-  #   Logger.info("Server running on port 4000")
-  #   {:ok, _} = Plug.Adapters.Cowboy.http(Horoscope.Router, [])
-  # end
 
   defdelegate get, to: Horoscope.Worker, as: :call
   defdelegate get(params), to: Horoscope.Worker, as: :call
--- mix.exs
+++ mix.exs
@@ -19,16 +19,13 @@ defmodule Horoscope.Mixfile do
   end
 
   defp deps do
-    [{:cowboy, "~> 1.0"},
-     {:plug, "~> 1.0"},
-     {:poolboy, "~> 1.5"},
+    [{:poolboy, "~> 1.5"},
      {:httpoison, "~> 0.7"},
      {:poison, "~> 1.5"},
      {:floki, "~> 0.3"},
      {:ecto, "~> 1.0"},
      {:postgrex, "~> 0.9"},
      {:towel, "~> 0.2"},
-     {:quantum, "~> 1.4"},
-     {:churchill, github: "knrz/churchill"}]
+     {:quantum, "~> 1.4"}]
   end
 end
